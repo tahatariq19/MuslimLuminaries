907d6f16ba2690ece9af2b174b4c344f99b44554
tahatariq19 <tahatariq19@protonmail.com>
2025-07-14 04:45:49 +0500

    Add: quotes.js with example quotes

---
+++ js/quotes.js
@@ -0,0 +1,19 @@
+// Example quotes of Muslim Scholars, Thinkers, and Philosophers
+const quotes = [
+  {
+    text: "He has so many creations, and yet He never forgets me, but I only have One creator, and I've forgotten Him countless of times.",
+    author: "Saadi"
+  },
+  {
+    text: "If a gem falls into mud it is still valuable. If dust ascends to heaven, it remains valueless.",
+    author: "Saadi"
+  },
+  {
+    text: "If the heart becomes hardened, the eye becomes dry.",
+    author: "Ibn al-Qayyim"
+  },
+  {
+    text: "A calamity that makes you turn to Allah is better for you than a blessing which makes you forget the remembrance of Allah.",
+    author: "Ibn Taymiyyah"
+  }
+];
