c8ff77e197fb8dc41729bc0458fb6b7d5ab09161
tahatariq19 <tahatariq19@protonmail.com>
2026-07-31 11:07:35 +0500

    add vite config and GitHub Pages deploy workflow

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	// Repo is at https://github.com/tahatariq19/MuslimLuminaries
+	// Deployed to https://tahatariq19.github.io/MuslimLuminaries/
+	base: '/MuslimLuminaries/',
+})
